043a603823d08205cd4467750fc37d101fd322b5
Marvin H. Linares Caridad <marvinhlc@gmail.com>
2022-07-28 16:38:13 -0600

    eslint
    eslint congif

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Box,Typography} from "@mui/material";
+
+export const Home: React.FC = () => {
+    return (
+        <Box>
+            <Typography variant="h4" component="h1">
+            HOME
+            </Typography>
+        </Box>
+    );
+};
+
+export default Home;
