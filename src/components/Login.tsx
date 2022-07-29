d02c86f661b3ff69f250224d53ddd74a5f50c2ca
Marvin H. Linares Caridad <marvinhlc@gmail.com>
2022-07-28 18:29:42 -0600

    login
    working...

---
+++ src/components/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {
+    onLogin: () => void;
+}
+
+const Login: React.FC<Props> = () => {
+
+    return (
+        <h1>LOGIN</h1>
+    );
+}
+
+export default Login;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,35 @@
-import React from "react";
-//import {Box,Typography} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {Box,Typography} from "@mui/material";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "../store/store";
+import { selectUser } from "../reducers/user";
+import Login from "../components/Login";
 
 export const Home: React.FC = () => {
+    const [tokenSession,setTokenSession] = useState<null | string>(null);
+    const dispatch = useDispatch();
+    const token = useAppSelector(selectUser);
+
+    useEffect(() => {
+        console.log('token->',token)
+    },[]);
+
+    const handleOnLogin = () => {
+        console.log('login...')
+    }
+
+    if(token.valor === ''){
+        return (
+            <Login onLogin={handleOnLogin} />
+        )
+    }
 
     return (
-        <div>
-            HOME
-        </div>
+        <Box>
+            <Typography variant="h5">
+                HOME PAGE
+            </Typography>
+        </Box>
     );
 };
 
